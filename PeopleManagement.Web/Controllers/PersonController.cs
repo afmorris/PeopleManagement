4ea2649691e423acf04dff16309c4720e3e878ea
Tony Morris <anthony.frederick.morris@gmail.com>
2018-08-12 18:10:18 -0400

    Update to person management

---
+++ PeopleManagement.Web/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManagement.Data.Interfaces;
+
+namespace PeopleManagement.Web.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository personRepository;
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            this.personRepository = personRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
